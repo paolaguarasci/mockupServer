38762541fd6f2a08e0a6791150b723f390bb69d4
Paola Guarasci <paolaguarasci@gmail.com>
2019-09-13 17:56:49 +0200

    add fileupload handle

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
+const formidable = require("formidable");
 const PORT = 5000;
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+app.post("/upload", (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    console.log(files);
+    var oldpath = files.cv.path;
+    var newpath = __dirname + "/uploaded/" + files.cv.name;
+    fs.rename(oldpath, newpath, function(err) {
+      if (err) throw err;
+      res.sendStatus(200);
+      res.end();
+    });
+  });
 });
 
 http.listen(PORT, () => {
